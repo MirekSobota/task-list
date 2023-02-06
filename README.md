dce0fcdaafa6d93af2b7082492302612b2a69c60
MirekSobota <miroslaw.sobota@gmail.com>
2023-02-06 13:54:31 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Task-list
+
+##Demo
+ https://mireksobota.github.io/Task-list-v.2.0/
+
+
+### Description
+Simple task list, you can add tasks mark it as a done or remove it from list.
+
+### Technologies
+- HTML
+- JavaScript (ES6)
+- CSS
+- Grid
+- Flex
+- BEM
+
---
+++ js/scrypt.js
@@ -0,0 +1,83 @@
+{
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+           });
+           
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+             <li class="taskList">                   
+            <button class="js-done button__done ">
+             ${task.done ? "✔" : ""}  
+            </button>            
+            <span ${task.done ? " class=\"taskDoneLineThrough\"" : ""}>
+            ${task.content}
+            </span>
+            <button class="js-removeTask button__removeTask">🗑</button>        
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButtons, index) => {
+            toggleDoneButtons.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+
+        const removeButtons = document.querySelectorAll(".js-removeTask");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTaskInput").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+
+        document.querySelector(".js-newTaskInput").focus();
+        document.querySelector(".js-newTaskInput").value = ""; 
+        };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit)
+    };
+
+    init();
+
+}
